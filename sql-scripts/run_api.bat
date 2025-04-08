142acfa11f701068f25259e995bbb1f8a9b2a557
lehaibien <lehaibien321@gmail.com>
2025-04-08 15:11:10 +0700

    refactor: update database name from BaseProject to AioTech
    This commit updates the database name across multiple configuration files and scripts to reflect the new project name, ensuring consistency throughout the codebase.

--- sql-scripts/run_api.bat
+++ sql-scripts/run_api.bat
@@ -1,5 +1,5 @@
 for %%f in (*.sql) do (
-    docker run --rm --network aiotech_api_only_dev_backend -v %cd%:/scripts -w /scripts mcr.microsoft.com/mssql-tools /opt/mssql-tools/bin/sqlcmd -S aiotech_db_only -U SA -P "S3cr3tP@ssw0rd" -d "BaseProject" -i "%%f"
+    docker run --rm --network aiotech_api_only_dev_backend -v %cd%:/scripts -w /scripts mcr.microsoft.com/mssql-tools /opt/mssql-tools/bin/sqlcmd -S aiotech_db_only -U SA -P "S3cr3tP@ssw0rd" -d "AioTech" -i "%%f"
 )
 
 pause
--- sql-scripts/script.sh
+++ sql-scripts/script.sh
@@ -14,13 +14,13 @@ fi
 
 # Run init.sql first
 echo "Running init.sql..."
-docker run --rm --network aiotech_prod_backend -v $(pwd):/scripts -w /scripts mcr.microsoft.com/mssql-tools /opt/mssql-tools/bin/sqlcmd -S aiotech_db -U SA -P "S3cr3tP@ssw0rd" -d "BaseProject" -i "init.sql"
+docker run --rm --network aiotech_prod_backend -v $(pwd):/scripts -w /scripts mcr.microsoft.com/mssql-tools /opt/mssql-tools/bin/sqlcmd -S aiotech_db -U SA -P "S3cr3tP@ssw0rd" -d "AioTech" -i "init.sql"
 
 # Run remaining SQL files
 for file in *.sql; do
     if [ "$file" != "init.sql" ]; then
         echo "Running $file..."
-        docker run --rm --network aiotech_prod_backend -v $(pwd):/scripts -w /scripts mcr.microsoft.com/mssql-tools /opt/mssql-tools/bin/sqlcmd -S aiotech_db -U SA -P "S3cr3tP@ssw0rd" -d "BaseProject" -i "$file"
+        docker run --rm --network aiotech_prod_backend -v $(pwd):/scripts -w /scripts mcr.microsoft.com/mssql-tools /opt/mssql-tools/bin/sqlcmd -S aiotech_db -U SA -P "S3cr3tP@ssw0rd" -d "AioTech" -i "$file"
     fi
 done
 
